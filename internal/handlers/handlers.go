e0bd054a4aa1b0b38b17f2a69dc4befd80909cf5
Dang Khyu Manh <d4ng777@yandex.ru>
2026-04-25 19:02:00 +0300

    fixed-according-review

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -22,7 +22,7 @@ func Get(w http.ResponseWriter, r *http.Request) {
 func Upload(w http.ResponseWriter, r *http.Request) {
 	file, header, err := r.FormFile("myFile")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
